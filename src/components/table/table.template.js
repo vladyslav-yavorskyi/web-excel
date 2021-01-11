083c6130acbd4a0253e90412d102fd566e0dcbc5
vladyslav-yavorskyi <57500942+vladyslav-yavorskyi@users.noreply.github.com>
2021-01-11 12:36:42 +0100

    Finish base framework functional (#3)

---
+++ src/components/table/table.template.js
@@ -0,0 +1,66 @@
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+function toCell() {
+  return `<div class="cell" contenteditable></div>`;
+}
+
+function toColumn(col) {
+  return `<div class="column">${col}</div>`;
+}
+
+function createRow(number, content) {
+  return `
+    <div class="row">
+      <div class="row-info">${number ? number : ''}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `;
+}
+
+function toChar(el, index) {
+  return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rowCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1;
+  const rows = [];
+
+  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
+
+  rows.push(createRow(null, cols));
+
+  for (let i = 0; i < rowCount; i++) {
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
+  }
+
+  return rows.join('');
+}
+
+// let temp = `
+// <div class="row"></div>
+// <div class="row-info"></div>
+// <div class="row-data">
+
+//   <div class="column">B</div>
+//   <div class="column">C</div>
+//   <div class="column">A</div>
+//   <div class="column">B</div>
+//   <div class="column">C</div>
+//   <div class="column">A</div>
+//   <div class="column">B</div>
+//   <div class="column">C</div>
+//   <div class="column">A</div>
+// </div>
+// </div>
+// <div class="row">
+// <div class="row-info">1</div>
+// <div class="row-data">
+//   <div class="cell selected" contenteditable="">A1</div>
+
+//   <div class="cell" contenteditable="">C3</div>
+// </div>
+// `;
